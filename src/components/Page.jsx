97ce5f4d6255c1099e6e2bfc3864272d2ff635e1
rirefat <rirefat.official@gmail.com>
2024-02-16 02:39:14 +0600

    complete weather main dashboard ui

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,13 @@
 import Header from "./Header/Header";
 import WeatherBoard from "./WeatherBoard/WeatherBoard";
+import bodyBG from '../assets/body-bg.png';
 
 const Page = () => {
     return (
-        <>
+        <main className={`bg-body font-[Roboto] text-light bg-[${bodyBG}] bg-no-repeat bg-cover h-screen grid place-items-center`}>
             <Header/>
             <WeatherBoard/>
-        </>
+        </main>
     );
 };
 
